d9adfd68965672c64754a7fd82b69fb5bc90c7bb
ryo0223 <ryo0223@users.noreply.github.com>
2019-03-04 00:13:08 +0000

    Create replace.py

---
+++ replace.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+filename=raw_input()
+query1=pd.read_csv("/home/vagrant/assignment1/"+filename)
+
+query1['Body']=query1['Body'].replace("\n","",regex=True)
+query1['Body']=query1['Body'].replace(",","",regex=True)
+outputname=raw_input()
+query1.to_csv(outputname+".csv",header=False)
